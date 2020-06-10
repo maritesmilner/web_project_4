d46a12d5a4b6a6c5a330797bf158c6c9679794d5
marites.milner <maritesmilner@gmail.com>
2020-06-09 22:53:03 -0400

    feat: added new place form

--- script.js
+++ script.js
@@ -1,69 +1,89 @@
 //Load 6 initial cards
 const initialCards = [
   {
-      name: "Yosemite Valley",
-      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
   },
   {
-      name: "Lake Louise",
-      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
   },
   {
-      name: "Bald Mountains",
-      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
   },
   {
-      name: "Latemar",
-      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+},
+  {
+    name: "Vanois National Park",
+    link: "https://code.s3.yandex.net/web-code/vanois.jpg"
   },
   {
-      name: "Vanois National Park",
-      link: "https://code.s3.yandex.net/web-code/vanois.jpg"
-  },
-  {
-      name: "Lago di Braies",
-      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
   }
 ];
 const placeTemplate = document.getElementById('place-template');
 const container = document.querySelector('.content');
 const places = container.querySelector('.places');
-initialCards.forEach(function (card) {
+const newPlaces = initialCards.map(card => makeCard(card));
+places.append(...newPlaces);
+
+//Edit Profile Form
+const editProfileForm = container.querySelector('.edit-profile-form');
+const editButton = container.querySelector('.edit-button');
+const edfCloseButton = editProfileForm.querySelector('.form__close-button');
+const edfSaveButton = editProfileForm.querySelector('.form__save-button');
+const profileName = container.querySelector('.profile__name');
+const profileTitle = container.querySelector('.profile__title');
+const profileNameField = editProfileForm.querySelector('.form__input_type_name');
+const profileTitleField = editProfileForm.querySelector('.form__input_type_title');
+editButton.addEventListener('click', function () {
+  profileNameField.value = profileName.textContent;
+  profileTitleField.value = profileTitle.textContent;
+  toggleForm(editProfileForm);
+});
+edfSaveButton.addEventListener('click', function () {
+  profileName.textContent = profileNameField.value;
+  profileTitle.textContent = profileTitleField.value;
+  toggleForm(editProfileForm);
+});
+edfCloseButton.addEventListener('click', function () { toggleForm(editProfileForm); });
+
+//New Place Form
+const addButton = container.querySelector('.add-button');
+const newPlaceForm = container.querySelector('.new-place-form');
+const npfCloseButton = newPlaceForm.querySelector('.form__close-button');
+const npfSaveButton = newPlaceForm.querySelector('.form__save-button');
+addButton.addEventListener('click', function () { toggleForm(newPlaceForm); });
+npfCloseButton.addEventListener('click', function () { toggleForm(newPlaceForm); });
+npfSaveButton.addEventListener('click', function () {
+  let name = newPlaceForm.querySelector('.form__input_type_title');
+  let link = newPlaceForm.querySelector('.form__input_type_link');
+  const card = {
+    name: name.value,
+    link: link.value
+  };
+  places.prepend(makeCard(card));
+  name.value = "";
+  link.value = "";
+  toggleForm(newPlaceForm);
+});
+
+//Shared functions
+function toggleForm(form) {
+  form.classList.toggle('form_hide');
+}
+function makeCard(card) {
   const newPlace = placeTemplate.content.cloneNode(true);
   const placePic = newPlace.querySelector('.place__picture');
   placePic.src = card.link;
   placePic.alt = `Picture of ${card.name}`;
   const placeName = newPlace.querySelector('.place__name');
   placeName.textContent = card.name;
-  places.appendChild(newPlace);
-})
-
-//Edit Profile Form functions
-const profileName = container.querySelector('.profile__name');
-const profileTitle = container.querySelector('.profile__title');
-const editButton = container.querySelector('.edit-button');
-const editProfileForm = container.querySelector('.edit-profile-form');
-const edfSaveButton = editProfileForm.querySelector('.form__save-button');
-const edfCloseButton = editProfileForm.querySelector('.form__close-button');
-const nameField = editProfileForm.querySelector('.form__input_type_name');
-const titleField = editProfileForm.querySelector('.form__input_type_title');
-
-function dislayEditForm() {
-  nameField.value = profileName.innerHTML;
-  titleField.value = profileTitle.innerHTML;
-  toggleEditForm();
+  return newPlace;
 }
-function toggleEditForm() {
-  editProfileForm.classList.toggle('edit-profile-form_hide');
-}
-function saveProfile() {
-  profileName.textContent = nameField.value;
-  profileTitle.textContent = titleField.value;
-  toggleEditForm();
-}
-
-editButton.addEventListener('click', dislayEditForm);
-edfCloseButton.addEventListener('click', toggleEditForm);
-edfSaveButton.addEventListener('click', saveProfile);
 
 
